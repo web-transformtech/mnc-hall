01290042c58a06db5ca68f12e729e5933d028799
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2024-06-05 15:11:48 +0530

    responsive

--- src/Components/Home/HighLight.jsx
+++ src/Components/Home/HighLight.jsx
@@ -1,13 +1,13 @@
 export default function HighLight() {
     return (
-        <section className="py-10 pb-32">
+        <section className="pb-32">
             <div className="relative h-ful">
-                <div className="absolute h-full bg-[#3f3f3f8c] w-full top-0 py-32">
-                    <div className="container mx-auto h-ful">
+                <div className="absolute h-full bg-[#3f3f3f8c] w-full top-0 py-32 sm:py-20">
+                    <div className="container mx-auto h-full">
                         <h1 className="lg:text-[64px] lge:text-[64px] md:text-[40px] mdsm:text-[40px] sm:text-[40px] Macondo pb-6 text-center sm:px-2  text-secondary font-semibold">
                             Celebration Hub
                         </h1>
-                        <h2 className="lg:text-[50px] lge:text-[50px] md:text-h1 mdsm:text-h1 sm:text-h3 Macondo w-[70%] mx-auto text-white font-semibold text-center">
+                        <h2 className="lg:text-[50px] lge:text-[50px] md:text-h1 mdsm:text-h1 sm:text-h4 Macondo w-[70%] mx-auto text-white font-semibold text-center">
                             &#34;Experience timeless elegance at our versatile event venue, perfect for weddings, receptions, and all your special
                             occasions. Book now and create unforgettable memories in a setting tailored to your dreams.&#34;
                         </h2>
